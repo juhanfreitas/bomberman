f2af290057c9625b9b27c29869a054d5f2e56eb2
juhanfreitas <juhanfreitas@gmail.com>
2024-08-24 21:33:25 -0300

    feat: adding the scoreboard

---
+++ bomberman/Bomberman/src/Game/Scoreboard.cpp
@@ -0,0 +1,74 @@
+/**********************************************************************************
+// Scoreboard (Código Fonte)
+//
+// Criação:     27 Jan 2013
+// Atualização: 12 Mar 2023
+// Compilador:  Visual C++ 2022
+//
+// Descrição:   Objeto animado
+//
+**********************************************************************************/
+
+#include "Scoreboard.h"
+
+string Scoreboard::score;
+string Scoreboard::timer;
+string Scoreboard::lives;
+string Scoreboard::bombs;
+string Scoreboard::power;
+
+
+Scoreboard::Scoreboard()
+{
+	scoreboard = new TileSet("Resources/scoreboard.png", 272, 32, 2, 6);
+	anim = new Animation(scoreboard, 0.750f, true);
+
+	string timer;
+
+	scoreboardHeight = scoreboard->TileHeight();
+
+	uint SeqRunning[4] = { 0, 1, 2, 3 };
+	uint SeqVictory[1] = { 4 };
+	uint SeqDefeat[1] = { 5 };
+
+	anim->Add(RUNNING, SeqRunning, 4);
+	anim->Add(VICTORY, SeqVictory, 1);
+	anim->Add(DEFEAT, SeqDefeat, 1);
+
+	state = RUNNING;
+
+	MoveTo(
+		window->CenterX(),
+		scoreboard->TileHeight() / 2.0f
+	);
+
+	font = new Font("Resources/Tahoma10.png");
+	font->Spacing("Resources/Tahoma10.dat");
+
+}
+
+Scoreboard::~Scoreboard()
+{
+	delete scoreboard;
+	delete anim;
+}
+
+void Scoreboard::Update()
+{
+	anim->Select(state);
+	anim->NextFrame();
+}
+void Scoreboard::Draw()
+{
+	anim->Draw(
+		window->CenterX() - scoreboard->TileWidth() / 2.0f,
+		scoreboard->TileHeight() / 2.0f,
+		Layer::FRONT
+	);
+	font->Draw(59, scoreboardHeight / 2.0f + 9, Scoreboard::score);
+	font->Draw((scoreboard->TileWidth() / 2.0f) - 6, scoreboardHeight / 2.0f + 9, Scoreboard::timer);
+	font->Draw((scoreboard->TileWidth() / 2.0f) + 50, scoreboardHeight / 2.0f + 9, Scoreboard::lives);
+	font->Draw((scoreboard->TileWidth() / 2.0f) + 90, scoreboardHeight / 2.0f + 9, Scoreboard::bombs);
+	font->Draw((scoreboard->TileWidth() / 2.0f) + 124, scoreboardHeight / 2.0f + 9, Scoreboard::power);
+		
+}
---
+++ bomberman/Bomberman/src/Game/Scoreboard.h
@@ -0,0 +1,117 @@
+/**********************************************************************************
+// Player (Arquivo de Cabeçalho)
+//
+// Criação:     27 Jan 2013
+// Atualização: 12 Mar 2023
+// Compilador:  Visual C++ 2022
+//
+// Descrição:   Objeto animado
+//
+**********************************************************************************/
+
+#ifndef _BOMBERMAN_SCOREBOARD_H_
+#define _BOMBERMAN_SCOREBOARD_H_
+
+// ---------------------------------------------------------------------------------
+// Inclusões
+
+#include "../Engine/Types.h"                      // tipos específicos da engine
+#include "../Engine/Object.h"                     // interface de Object
+#include "../Engine/Animation.h"                  // animação de sprites
+#include "../Engine/Font.h"
+
+#include <sstream>
+#include <iomanip>
+
+// ------------------------------------------------------------------------------
+
+enum TimerState { RUNNING, VICTORY, DEFEAT };
+
+// ---------------------------------------------------------------------------------
+
+class Scoreboard : public Object
+{
+private:
+    TileSet* scoreboard;                // folha de sprites do personagem
+    Animation* anim;                   // animação do personagem
+    Font * font;
+    static string score;
+    static string timer;
+    static string lives;
+    static string bombs;
+    static string power;
+
+public:
+    uint state;                         // estado atual do personagem
+    uint scoreboardHeight;
+
+    Scoreboard();                             // construtor
+    ~Scoreboard();                            // destrutor
+
+    static void UpdateScore(uint score);
+    static void UpdateTimer(uint timeLimit, uint gameTime);
+    static void UpdateLives(uint lives);
+	static void UpdateBombs(uint bombs);
+	static void UpdatePower(uint power);
+
+    void Update();                      // atualização do objeto
+    void Draw();                        // desenho do objeto
+
+    uint Height() const;
+	static std::string SetBoardText(uint value, uint lenght);
+	static std::string SetBoardText(uint value, uint secondValue, string separator, uint lenght);
+};
+
+// ---------------------------------------------------------------------------------
+// Função Membro Inline
+
+// retorna a altura do objeto
+
+inline uint Scoreboard::Height() const
+{
+	return scoreboardHeight;
+}
+
+inline std::string Scoreboard::SetBoardText(
+	uint value,
+	uint lenght)
+{
+	std::stringstream ss;
+	ss << std::setw(lenght) << std::setfill('0') << value;
+	return ss.str();
+}
+
+inline std::string Scoreboard::SetBoardText(
+	uint firstValue,
+	uint secondValue,
+	string separator,
+	uint lenght)
+{
+	std::stringstream ss;
+	ss << firstValue << separator << std::setw(lenght) << std::setfill('0') << secondValue;
+	return ss.str();
+}
+
+inline void Scoreboard::UpdateScore(uint newScore)
+{
+	Scoreboard::score = SetBoardText(newScore, 5);
+}
+inline void Scoreboard::UpdateTimer(uint timeLimit, uint gameTime) {
+	uint remainingTime = timeLimit - gameTime;
+	uint minutes = remainingTime / 60;
+	uint seconds = remainingTime % 60;
+	Scoreboard::timer = SetBoardText(minutes, seconds, ":", 2);
+}
+inline void Scoreboard::UpdateLives(uint lives) {
+	Scoreboard::lives = SetBoardText(lives, 2);
+}
+inline void Scoreboard::UpdateBombs(uint bombs) {
+	Scoreboard::bombs = SetBoardText(bombs, 1);
+}
+inline void Scoreboard::UpdatePower(uint power) {
+	Scoreboard::power = SetBoardText(power, 1);
+}
+
+// ---------------------------------------------------------------------------------
+
+#endif
