2027068c9e5ba106f8f2047fb7cfae26f7a74483
juhanfreitas <juhanfreitas@gmail.com>
2024-08-26 22:53:44 -0300

    feat: creating bombs

--- bomberman/Bomberman/src/Game/Bomb.h
+++ bomberman/Bomberman/src/Game/Bomb.h
@@ -9,34 +9,33 @@
 #include "../Engine/Sprite.h"
 #include "../Engine/TileSet.h"
 #include "../Engine/Animation.h"
+#include "../Engine/Timer.h"
 
 // ---------------------------------------------------------
 
-enum BombType {NORMAL, SPIKE};
+enum BombType { NORMAL, SPIKE };
 
 class Bomb : public Object
 {
 private:
 	TileSet * bombs = nullptr;
 	Animation * anim = nullptr;
+	bool hasCollision = false;
 
 public:
 
-	BombType type;
+	Timer timer;
+	BombType bombMode;
 
-	Bomb(BombType Bombtype, float x, float y);
+	Bomb(BombType Bombtype, float playerX, float playerY);
 	~Bomb();
 
 	void Update();
 	void Draw();
+	void CheckPlayerPosition();
 };
 
-inline void Bomb::Update()
-{
-	anim->NextFrame();
-}
-
 inline void Bomb::Draw() 
-{anim->Draw(x, y);}
+{ anim->Draw(x, y, z); }
 
 #endif _BOMB_H_
